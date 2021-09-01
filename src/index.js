15db7b5fe9ebdacf6aa5f852af5ea1615337d1d3
Mxx1029 <lia.pim@icloud.com>
2021-09-01 16:49:16 +0200

    Try to use .env

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import process from 'process';
 
 const [ node, script, city] = process.argv;
 
+// const apikey = process.env.OPEN_WEATHER_API_KEY;
 const apikey = '4d117631921f7c71863a35c91745442a';
 
 axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`)
